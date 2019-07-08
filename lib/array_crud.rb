0e5517ba57e0108337919cab2b151f8076a9fd45
Olivia Kesler <oakesler@protonmail.ch>
2019-07-08 06:19:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [lala, denimchicken, unicorns, sushi]
+  ["worms", "denimchickens", "unicorns", "sushi"]
 end
 
 def add_element_to_end_of_array(array, element)
