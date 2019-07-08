1231f914547ad0ac1f4fdf7c8dae7a95719d6980
Olivia Kesler <oakesler@protonmail.ch>
2019-07-08 05:48:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  []
+  array = []
 end
 
 def create_an_array
-  new_array = [worm, denimchicken, unicorns, sushi]
+  array = [worm, denimchicken, unicorns, sushi]
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array >> "oyamel"
+  us_stuff >> "oyamel"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  us_stuff.unshift("uber eats")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.shift[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
